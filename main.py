5c939714c844349e12844bf00d801192f49ad8e2
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-08-13 14:26:54 -0500

    update gui

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ root.title("Stephen Typing Test")
 main_frame = ttk.Frame(root, padding=10, width=950, height=350)
 main_frame.grid(row=0, column=0)
 secondary_frame = ttk.Frame(root, padding=10, width=950, height=350)
-secondary_frame.grid(row=1, column=0)
+secondary_frame.grid(row=2, column=0)
 tertiary_frame = ttk.Frame(root, padding=10, width=950, height=350)
-tertiary_frame.grid(row=2, column=0)
+tertiary_frame.grid(row=3, column=0)
 
-results = "Well done!! Your typing speed is ____ words per minute!! "
+
+def start_typing():
+    pass
+
+
+results = "Well done!! Your typing speed is ____ words per minute!! \n\n" \
+          "You made ___ mistakes in your typing\n\n" \
+          "The mistakes you made are: "
 
 ipsum_lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" \
               " et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel. Nisl pretium fusce id " \
@@ -31,11 +38,14 @@ start_font = Font(family="Luminrari", size=20, weight="bold")
 words_font = Font(family="Luminrari", size=11, weight="normal")
 
 # ==============UI Design========================== #
-app_header = ttk.Label(main_frame, text="TEST YOUR TYPING SPEED", anchor="center", font=heading_font)
+app_header = ttk.Label(main_frame, text="TEST YOUR TYPING SPEED", anchor="center", font=heading_font, padding=15)
 app_header.grid(row=0, column=0, columnspan=4)
 
-start_label = ttk.Label(main_frame, text="Click >>>Here<<< To Start", anchor="center", font=start_font, padding=10)
-start_label.grid(row=1, column=0, columnspan=4)
+# start_label = ttk.Label(main_frame, text="Click", anchor="center", font=start_font, padding=10)
+# start_label.grid(row=1, column=0)
+
+start_button = ttk.Button(main_frame, text=">>>Click To Start<<<", command=start_typing)
+start_button.grid(row=1, column=1)
 
 words_label = ttk.Label(main_frame, text=ipsum_lorem, font=words_font, padding=10, relief="solid", wraplength=550)
 words_label.grid(row=2, column=0, columnspan=4)
@@ -49,11 +59,7 @@ timer_label.grid(row=1, column=0)
 wpm_label = ttk.Label(secondary_frame, text="wpm: placeholder", anchor="nw")
 wpm_label.grid(row=1, column=2)
 
-results_label = ttk.Label(tertiary_frame, text=results, padding=10, relief="solid", wraplength=550, width=100)
+results_label = ttk.Label(tertiary_frame, text=results, padding=10, relief="solid", width=75, justify="left")
 results_label.grid(row=0, column=0)
-
-
-
-
 
 root.mainloop()
